473555a7ee453d3be2e09c4d59755bdeca792742
Ben Friedman <ben@macmini01.local>
2020-04-24 16:31:16 -0400

    some params and readme

--- stockinator.py
+++ stockinator.py
@@ -54,13 +54,17 @@ def sort_list_of_tuples(l, reverse=True):
 
 if __name__ == '__main__':
     tickers = ['SPY', 'VOX', 'VCR', 'VDC', 'VDE', 'VFH', 'VHT', 'VIS', 'VGT', 'VAW', 'VNQ', 'VPU']
+    days_back = [233, 144, 89, 55, 34, 21]
     data = get_stock_data(tickers, period='2y', interval='1d')
     
-    raw_perf_data = get_performance(data, periods=[144, 89, 55, 34, 21, 8, 5])
-    weighed_perf_data = get_weighted_performance(raw_perf_data, weight=5)
+    raw_perf_data = get_performance(data, periods=days_back)
+    weighed_perf_data = get_weighted_performance(raw_perf_data, weight=days_back[-1])
     new_stocks = get_new_investments(weighed_perf_data)
 
-    print(f"Sector strength according to STOCKINATOR as of {datetime.datetime.now()}")
+    print("Sector strength according to STOCKINATOR")
+    print(f"Date of report: {datetime.datetime.now()}")
+    print(f"Intervals and weighting used: {days_back}")
+
     for stock in sort_list_of_tuples(new_stocks):
         print(stock[0], stock[1])
 
