bcb69498aaf1eadf413da1d0061745af51e73559
Ben Friedman <silver8ack@gmail.com>
2020-04-26 17:23:41 -0400

    idk

--- stockinator.py
+++ stockinator.py
@@ -8,14 +8,6 @@ import csv
 import os
 from collections import OrderedDict
 
-def unique_list(l):
-    list_set = set(l) 
-    return list(list_set)
-        
-def get_sp_companies():
-    data = pd.read_csv('sp500.csv')
-    return data
-    
 # get stock data from yahoo finance
 def get_stock_data(tickers, start=None, end=None, interval='1d', period=None):
     return yf.download(
@@ -29,87 +21,52 @@ def get_stock_data(tickers, start=None, end=None, interval='1d', period=None):
         threads=True
     )
 
+def unique_list(l):
+    list_set = set(l) 
+    return list(list_set)
+
+def get_sp_companies():
+    data = pd.read_csv('sp500.csv')
+    return data
+
 def get_period_performance(data, periods=[360], stat='Close', offset=0, weight=1):
     tickers = unique_list([x[0] for x in data.columns.values])
-    columns = periods
+    columns = periods.copy()
     if offset > 0:
         columns += [f"Next_{offset}"]
     df = pd.DataFrame(columns=columns, index=tickers)
 
-    for ticker in tickers:
-        period_pctchanges = []
-        for i, period in enumerate(periods):
-            if offset > 0:
-                chunk = data.iloc[-(period+offset):-offset]
-            else:
-                chunk = data.iloc[-(period+offset):]
-            
-            first_close = chunk[ticker].iloc[0]['Close']
-            last_close = chunk[ticker].iloc[-1]['Close']
-            period_pctchanges.append(((last_close - first_close) / first_close) * 100)
-         
-        df.loc[ticker] = period_pctchanges
-        display(df)
-            
-    
-# calculate performance data from dataframes
-# using the periods and offset
-def get_performance(data, periods=[360], stat='Close', offset=0):
-    perf_data = {}
     for i, period in enumerate(periods):
-        for t, d in data.iteritems():
-            if t[1] == stat:
-                chunk = None
-                if offset > 0:
-                    chunk = d[-(period+offset):-offset]
-                else:
-                    chunk = d[-(period+offset):]
+        if offset > 0:
+            chunk = data.iloc[-(period + offset):-offset]
+        else:
+            chunk = data.iloc[-period:]
 
-                if not t[0] in perf_data:
-                    perf_data[t[0]] = OrderedDict()
+        for ticker in tickers:
+            first_close = chunk[ticker]['Close'].iloc[0]
+            last_close = chunk[ticker]['Close'].iloc[-1]
+            pct_performance = ((last_close - first_close) / first_close) * 100
+            df.at[ticker, period] = pct_performance
 
-                perf_data[t[0]][period] = \
-                    {'raw': (chunk[-1] - chunk[0]) / chunk[0] * 100}            
+            df.at[ticker, f"{period}_Weighted"] = pct_performance * (weight / period)
+
+            #check if offset is used and we're at the last data period
+            if offset > 0 and i == len(periods)-1:
+                next_chunk = data.iloc[-(offset):]
+                next_first_close = next_chunk[ticker]['Close'].iloc[0]
+                next_last_close = next_chunk[ticker]['Close'].iloc[-1] 
+                next_pct_performance = ((next_last_close - next_first_close) / next_first_close) * 100
+                df.at[ticker, f"Next_{offset}"] = next_pct_performance
+
+    period_weights = df.filter(regex='_Weighted')
+    df['TotalWeight'] = period_weights.sum(axis=1)
         
-                #check if offset is used and we're at the last data period
-                if offset > 0 and i == len(periods)-1:
-                    new_chunk = d[-(offset):]
-                    perf_data[t[0]][offset]['next_period'] = \
-                        (new_chunk[-1] - new_chunk[0]) / new_chunk[0] * 100
-
-    return perf_data
-
-# weight the performance data based on the periods used
-def get_weighted_performance(d, weight=1):
-    for ticker, period in d.items():
-        for period_key, perf_data in d[ticker].items():
-            d[ticker][period_key]['weighted'] = \
-                (weight / int(period_key)) * perf_data['raw']
-
-    return d
-
-# rank investements and calculate performance of next period
-def get_new_investments(d):
-    weighted_totals = []
-    for ticker, perf_data in d.items():
-        ticker_total = 0
-        next_period = 0
-        for period, data in perf_data.items():
-            ticker_total += data['weighted']
-            if 'next_period' in data:
-                next_period = data['next_period']
-        
-        weighted_totals.append((ticker, \
-            np.around(ticker_total, decimals=2), np.around(next_period, decimals=2,)))
-
-    return weighted_totals
-
-# sort tuples
-def sort_list_of_tuples(l, reverse=True):  
-    return(sorted(l, key = lambda x: x[1], reverse=reverse))
-
+    return df
 
 if __name__ == '__main__':
+    pd.set_option('display.max_rows', 1000)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 150)
     #tickers = [
     #    'SPY', #'VBR', 'VBK', 'VUG', 'VTV',
     #    'VOX', 'VCR', 'VDC', 'VDE', 'VFH', 'VHT', 'VIS', 'VGT', 'VAW', 'VNQ', 'VPU'
@@ -121,8 +78,8 @@ if __name__ == '__main__':
         #[144, 34, 8],
         #[34, 21, 13, 8, 5],
         #[89, 34, 13, 5],
-        #[144, 55, 21, 8],
-        [89, 21, 5],
+        [233, 89, 34, 13],
+        #[89, 21, 5],
     ]
     sp500 = get_sp_companies()
     #data = get_stock_data(tickers, period='2y', interval='1d')
@@ -142,16 +99,10 @@ if __name__ == '__main__':
         print()
         print(f"Intervals and weighting used: {days_back}")
         
-        offset = 0
+        #offset = 0
         #offset = days_back[-1]
-        #offset = days_back[len(days_back)-2]
-        raw_perf_data = get_performance(data, periods=days_back, offset=offset)
-        weighted_perf_data = get_weighted_performance(raw_perf_data, weight=days_back[-1])
-        new_stocks = get_new_investments(weighted_perf_data)
-        
-        print(f"Ticker, Rank, Performance Next {offset}")
-        sorted_stocks = sort_list_of_tuples(new_stocks)
-        for stock in sorted_stocks:
-            print(f"{stock[0]}, {stock[1]}, {stock[2]}")
+        offset = days_back[len(days_back)-2]
+        pf_data = get_period_performance(data, data_set_intervals[-1], offset=offset, weight=days_back[-1])
+        print(pf_data.sort_values('TotalWeight', ascending=False))  
 
 
