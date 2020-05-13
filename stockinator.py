ece0d890abb51b57df6eb04576119b269af74cb5
Ben Friedman <silver8ack@gmail.com>
2020-05-12 22:18:58 -0400

    stuff

--- stockinator.py
+++ stockinator.py
@@ -1,11 +1,13 @@
-import talib as ta
-import matplotlib.pyplot as plt
-import requests
-import time
-import sys
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import talib as ta
+import matplotlib.pyplot as plt
+from matplotlib.pylab import date2num
+from mplfinance.original_flavor import candlestick_ohlc
+import requests
+import time
+import sys
 import datetime
 import json
 import requests
@@ -23,7 +25,7 @@ def get_stock_data(tickers, start=None, end=None, interval='1d', period=None):
         interval=interval,
         group_by="ticker",
         auto_adjust=False,
-        threads=False
+        threads=True
     )
 
 def unique_list(l):
@@ -72,6 +74,58 @@ def calculate_mfi(df, period=14):
 def get_sp_companies():
     data = pd.read_csv('stocks/sp500.csv')
     return data
+
+def plot_chart(data, n, ticker):
+    
+    # Filter number of observations to plot
+    data = data.iloc[-n:]
+    
+    # Create figure and set axes for subplots
+    fig = plt.figure()
+    fig.set_size_inches((20, 16))
+    ax_candle = fig.add_axes((0, 0.72, 1, 0.32))
+    #ax_macd = fig.add_axes((0, 0.48, 1, 0.2), sharex=ax_candle)
+    ax_rsi = fig.add_axes((0, 0.24, 1, 0.2), sharex=ax_candle)
+    ax_vol = fig.add_axes((0, 0, 1, 0.2), sharex=ax_candle)
+    
+    # Format x-axis ticks as dates
+    ax_candle.xaxis_date()
+    
+    # Get nested list of date, open, high, low and close prices
+    ohlc = []
+    for date, row in data.iterrows():
+        openp, highp, lowp, closep = row[:4]
+        ohlc.append([date2num(date), openp, highp, lowp, closep])
+ 
+    # Plot candlestick chart
+    ax_candle.plot(data.index, data["EMA_1"], label="EMA_1")
+    ax_candle.plot(data.index, data["EMA_2"], label="EMA_2")
+    ax_candle.plot(data.index, data["EMA_3"], label="EMA_3")
+    candlestick_ohlc(ax_candle, ohlc, colorup="g", colordown="r", width=0.8)
+    ax_candle.legend()
+    
+    # Plot MACD
+    #ax_macd.plot(data.index, data["macd"], label="macd")
+    #ax_macd.bar(data.index, data["macd_hist"] * 3, label="hist")
+    #ax_macd.plot(data.index, data["macd_signal"], label="signal")
+    #ax_macd.legend()
+    
+    # Plot RSI
+    # Above 70% = overbought, below 30% = oversold
+    ax_rsi.set_ylabel("(%)")
+    ax_rsi.plot(data.index, [70] * len(data.index), label="overbought")
+    ax_rsi.plot(data.index, [30] * len(data.index), label="oversold")
+    ax_rsi.plot(data.index, data["RSI"], label="RSI")
+    ax_rsi.legend()
+    
+    # Show volume in millions
+    ax_vol.bar(data.index, data["Volume"] / 1000000)
+    ax_vol.set_ylabel("(Million)")
+   
+    # Save the chart as PNG
+    fig.savefig("charts/" + ticker + ".png", bbox_inches="tight")
+    
+    plt.show()
 
 def get_period_performance(data, periods=[360], offset=0, weight=1):
     tickers = unique_list([x[0] for x in data.columns.values])
